68a82f1b4f2e2a755e1657a81d332523f0a73635
Mark Lui Agbayani <markagbayanipersonal19@gmail.com>
2024-11-02 00:04:38 +0800

    Added a new features

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-function flipCoin(){
-    let coinToss = Math.floor(Math.random() * 2 );
-
-    if (coinToss === 0 ) {
-        document.getElementById("coin").setAttribute("src", "./images/head.png");
-        document.getElementById("h1").textContent = "Head";
+document.getElementById("button").addEventListener("click", function(){
+    let flip = Math.floor(Math.random() *2);
+    if (flip=== 0){
+    document.getElementById("coin").setAttribute("src", "./images/head.png")
+    document.getElementById("h1").textContent = "Head";
     }
     else{
-        document.getElementById("coin").setAttribute("src", "./images/tails.png");
-        document.getElementById("h1").textContent = "Tails";
+    document.getElementById("coin").setAttribute("src", "./images/tails.png")
+    document.getElementById("h1").textContent = "Tails";
     }
+});
+
+function spinAnimation(){
+    classList.add("spin");
+    setTimeout(function(){
+    classList.remover("spin");
+    }, 200)
 }
